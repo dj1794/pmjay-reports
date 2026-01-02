d97ecd591140ba720d8d995785bb8b17b6702ff6
Gaurav <er.gauravkulsh@gmail.com>
2026-01-02 23:46:43 +0530

    login page add

---
+++ Pmjay.Web/Data/LoginStateService.cs
@@ -0,0 +1,34 @@
+﻿namespace Pmjay.Web.Data
+{
+    using Microsoft.JSInterop;
+
+    public class LoginStateService
+    {
+        private readonly IJSRuntime _js;
+
+        public bool IsLoggedIn { get; private set; }
+
+        public LoginStateService(IJSRuntime js)
+        {
+            _js = js;
+        }
+
+        public async Task Login()
+        {
+            IsLoggedIn = true;
+            await _js.InvokeVoidAsync("localStorage.setItem", "isLoggedIn", "true");
+        }
+
+        public async Task LoadState()
+        {
+            var value = await _js.InvokeAsync<string>("localStorage.getItem", "isLoggedIn");
+            IsLoggedIn = value == "true";
+        }
+
+        public async Task Logout()
+        {
+            IsLoggedIn = false;
+            await _js.InvokeVoidAsync("localStorage.removeItem", "isLoggedIn");
+        }
+    }
+}
--- Pmjay.Web/Program.cs
+++ Pmjay.Web/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Pmjay.Web.ApiClients;
 using Pmjay.Web.Components;
+using Pmjay.Web.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,8 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var apiUrl = builder.Configuration.GetValue<string>("ApiUrl");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
-
 builder.Services.AddScoped<HomeApiClient>();
-
+builder.Services.AddSingleton<LoginStateService>();
 await builder.Build().RunAsync();
+
 
